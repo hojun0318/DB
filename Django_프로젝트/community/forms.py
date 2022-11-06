2655c1af5b36059e9ce64eebe68e5cddf1ed8510
hojun0318 <jaden8994@gmail.com>
2022-11-06 23:40:25 +0900

    community

---
+++ Django_프로젝트/community/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Review, ReviewComment
+
+
+class ReviewForm(forms.ModelForm):
+    
+    class Meta:
+        model = Review
+        fields = ['title', 'movie_title', 'rank', 'content']
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = ReviewComment
+        exclude = ['review', 'user']
--- Django_프로젝트/mypjt/settings.py
+++ Django_프로젝트/mypjt/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'movies',
     'accounts',
+    'community',
     
     'django.contrib.admin',
     'django.contrib.auth',
